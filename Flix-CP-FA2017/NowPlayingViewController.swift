b6aade33f02037ef05aedd82cca54c3a837962fb
Sean Nam <namsangwoo1@gmail.com>
2017-11-17 22:53:36 -0800

    Created network request

---
+++ Flix-CP-FA2017/NowPlayingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NowPlayingViewController.swift
+//  Flix-CP-FA2017
+//
+//  Created by Sean Nam on 11/17/17.
+//  Copyright © 2017 seannam. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+    override func viewDidLoad() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print(title)
+                }
+                // TODO: Get the array of movies
+                // TODO: Store the movies in a property to use elsewhere
+                // TODO: Reload your table view data
+                
+            }
+        }
+        task.resume()
+    }
+}
