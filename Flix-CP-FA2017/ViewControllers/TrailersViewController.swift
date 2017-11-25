92152bdfbffee51711a20e895fa2dedb9b397f80
Sean Nam <namsangwoo1@gmail.com>
2017-11-24 17:38:12 -0800

    Updated README and added demo video

---
+++ Flix-CP-FA2017/ViewControllers/TrailersViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TrailersViewController.swift
+//  Flix-CP-FA2017
+//
+//  Created by Sean Nam on 11/24/17.
+//  Copyright © 2017 seannam. All rights reserved.
+//
+
+import UIKit
+
+class TrailersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
