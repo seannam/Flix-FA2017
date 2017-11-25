3735d21932373603cece5a60e896f8eba68a4eed
Sean Nam <namsangwoo1@gmail.com>
2017-11-24 17:17:08 -0800

    Added CollectionView

---
+++ Flix-CP-FA2017/Views/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  Flix-CP-FA2017
+//
+//  Created by Sean Nam on 11/24/17.
+//  Copyright © 2017 seannam. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
